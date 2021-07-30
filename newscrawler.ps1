4ec3590b5d36dbb7cdd7e803c21b05d4744e9183
Nerfherder_Brown <47439495+nerfherderbrown@users.noreply.github.com>
2021-07-30 12:00:43 -0500

    Update newscrawler.ps1

--- newscrawler.ps1
+++ newscrawler.ps1
@@ -1,5 +1,5 @@
-$newscsv = './news.csv'
-$imported_articles = import-csv $newscsv
+$newstext = './news.txt'
+$imported_articles = get-content $newstext
 $HTML = Invoke-WebRequest -Uri 'https://cyware.com/cyber-security-news-articles'
 $test = $HTML.links | where innerHTML -Like "*cy-card__title m-0 cursor-pointer pb-3*" | select innerText, href
 $date = get-date -Format yyyyMMdd
@@ -7,7 +7,6 @@ $all_resources = @()
 foreach ($item in $test){
     if ($item.href -notlike "https://*"){$link = "https://cyware.com" + $item.href}
     else{$link = $item.href}
-    $link
     $truelink = $link.Substring(0, $link.IndexOf('?'))
     $temp_obj = new-object -TypeName psobject -Property @{
         Source = 'cyware.com'
@@ -15,39 +14,8 @@ foreach ($item in $test){
         Link = $truelink
         Date = $date
     }
-    if (!($imported_articles.link -contains $temp_obj.link)){
+    if (!($imported_articles -contains $temp_obj.link)){
         $all_resources += $temp_obj
-        $temp_obj | export-csv -NoTypeInformation $newscsv -Append
+        $temp_obj.link | Out-File $newstext -Append
     }
 }
-$test
-#if($all_resources){
-#    $ArrayTable = New-Object 'System.Collections.Generic.List[System.Object]'
-#    foreach ($article in $all_resources){
-#        $title = $article.title
-#        $link = $article.link
-#        $Section = @{
-#                        activityTitle = "$title"
-#                        activityText  = "$link"
-#        }
-#        $ArrayTable.add($section)
-#    }
-#    $JSONBody = [PSCustomObject][Ordered]@{
-#        "@type"      = "MessageCard"
-#        "@context"   = "http://schema.org/extensions"
-#        "summary"    = "News Stories"
-#        "themeColor" = '0078D7'
-#        "sections"   = $ArrayTable
-#    }
-#
-#    $Teamsbody = convertto-json $JSONBody -Depth 100
-#    $Teams_URL_SOC = ""
-#    $body = ConvertTo-Json -Depth 8 @{
-#                    title = "News Stories"
-#                    text  = "News Stories"
-#                    sections = $all_resources
-#
-#    }
-#    $TeamMessageBody = ConvertTo-Json $JSONBody -Depth 100
-#    Invoke-RestMethod -Method Post -ContentType application/json -Body $Teamsbody -uri $Teams_URL_SOC
-#}
